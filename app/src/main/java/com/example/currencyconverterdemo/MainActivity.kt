e28cd04eb11e92d6017f97354829f19870a7908b
LouisD-github <louis.dangendorf@edu.bib.de>
2024-12-17 17:31:48 +0100

    Upload

---
+++ app/src/main/java/com/example/currencyconverterdemo/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.currencyconverterdemo
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.ArrayAdapter
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val imgView: ImageView = findViewById(R.id.imgView)
+        imgView.setImageResource(R.drawable.exchange);
+
+        val spinnerIn: Spinner = findViewById(R.id.spinnerIn)
+        val spinnerOut: Spinner = findViewById(R.id.spinnerOut)
+        val textField: EditText = findViewById(R.id.textField)
+        val outputField: TextView = findViewById(R.id.outputCurrency)
+
+        ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            resources.getStringArray(R.array.optionsArr)
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerIn.adapter = adapter
+            spinnerOut.adapter = adapter
+        }
+        spinnerOut.setSelection(1)
+
+        textField.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int)
+            {
+                convert(spinnerIn, spinnerOut, textField, outputField)
+            }
+            override fun afterTextChanged(editable: Editable?) {}
+        })
+    }
+
+    private fun convert(spinnerIn: Spinner, spinnerOut: Spinner, textField: EditText, outputField: TextView) {
+        val input = spinnerIn.selectedItem.toString()
+        val output = spinnerOut.selectedItem.toString()
+
+        if (input == output) {
+            alert()
+            outputField.text = ""
+        }
+
+        val amountText = textField.text.toString()
+        val amount = amountText.toDoubleOrNull()
+        if (amount == null || amount <= 0) {
+            outputField.text = "Umrechnung ..."
+            return
+        }
+
+        val rate = getRate(input, output)
+        val result = amount * rate
+
+        outputField.text = String.format("%.2f %s", result, output)
+    }
+
+    private fun getRate(input: String, output: String): Double {
+        return when ("$input $output") {
+            "EUR EUR" -> 1.0
+            "EUR USD" -> 1.05
+            "EUR GBP" -> 0.83
+            "EUR JPY" -> 161.44
+
+            "USD EUR" -> 0.95
+            "USD USD" -> 1.0
+            "USD GBP" -> 0.79
+            "USD JPY" -> 154.09
+
+            "GBP EUR" -> 1.21
+            "GBP USD" -> 1.27
+            "GBP GBP" -> 1.0
+            "GBP JPY" -> 195.09
+
+            "JPY EUR" -> 0.0062
+            "JPY USD" -> 0.0065
+            "JPY GBP" -> 0.0051
+            "JPY JPY" -> 1.0
+
+            else -> 1.0
+        }
+    }
+
+    private fun alert() {
+        val msg = AlertDialog.Builder(this)
+        msg.setTitle("Ungültige Währungsauwahl")
+        msg.setMessage("Zwei Mal dieselbe Währung ausgewählt.")
+        msg.setPositiveButton("OK") { dialog, _ -> dialog.dismiss() }
+        msg.show()
+    }
+}
